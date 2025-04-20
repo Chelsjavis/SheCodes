eeca3e257a776c3e94baa72052206393ba17638d
Chelsjavis <chelsea.davis975@gmail.com>
2025-04-20 21:30:30 +1000

    Plus Project up to Week 5

---
+++ PlusProject/src/index.js
@@ -0,0 +1,52 @@
+function displayTemperature(response) {
+  let city = response.data.city;
+  console.log(city);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
+
+//let apiKey = "1894f4b60349tcab94fb26933d94a5o1";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Sydney&key=1894f4b60349tcab94fb26933d94a5o1";
+
+axios.get("apiUrl").then(displayTemperature);
